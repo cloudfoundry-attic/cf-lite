3188994d28e6d11298ebe2ed46da40616863fb13
Andrea Law <alaw@pivotal.io>
2015-01-13 16:46:02 -0500

    Use specified CF release
    Signed-off-by: Andrei Dinin <adinin@pivotal.io>

--- ci/virtualbox_box_build.sh
+++ ci/virtualbox_box_build.sh
@@ -2,6 +2,15 @@
 
 set -ex
 
+download_cf_release() {
+  cf_release_version=$1
+  cf_tgz=cf-${cf_release_version}.tgz
+
+  if [ ! -e $cf_tgz ]; then
+    wget --progress=bar https://s3.amazonaws.com/cf-lite-build-artifacts/$cf_tgz
+  fi
+}
+
 upload_box() {
   box_type=$1
   candidate_build_number=$2
@@ -11,6 +20,7 @@ upload_box() {
   s3cmd --access_key=$BOSH_AWS_ACCESS_KEY_ID --secret_key=$BOSH_AWS_SECRET_ACCESS_KEY put $box_name $bucket_url
 }
 
+download_cf_release ${CF_RELEASE_VERSION}
 ./bin/build-virtualbox
 upload_box virtualbox ${GO_PIPELINE_COUNTER}
 
