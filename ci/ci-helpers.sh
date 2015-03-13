68fbedda258ffab5aedf0421dc45632a2c8e5707
Thulasi Nandakumaran <tnandakumaran@pivotal.io>
2015-03-13 13:33:14 -0400

    GoCD environment seems to have changed; AWS creds are now in .s3cfg
    Signed-off-by: Graeme Davison <gdavison@pivotal.io>

--- ci/ci-helpers.sh
+++ ci/ci-helpers.sh
@@ -15,7 +15,7 @@ upload_box() {
 
   box_name=cf-lite-${box_type}-ubuntu-trusty-${candidate_build_number}.box
   bucket_url=s3://cf-lite-ci-pipeline/
-  s3cmd --access_key=$BOSH_AWS_ACCESS_KEY_ID --secret_key=$BOSH_AWS_SECRET_ACCESS_KEY put $box_name $bucket_url
+  s3cmd put $box_name $bucket_url
 }
 
 set_virtualbox_machine_folder() {
