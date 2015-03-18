d3b0508a3940c1e3ae9762402ff899a61b850418
Thulasi Nandakumaran <tnandakumaran@pivotal.io>
2015-03-18 11:20:23 -0400

    unset BOSH_LITE_SUBNET_ID so that automatic goCD setting doesn't interfere with CF-Lite AWS account setup
    Signed-off-by: Graeme Davison <gdavison@pivotal.io>

--- ci/aws_box_bin_test.sh
+++ ci/aws_box_bin_test.sh
@@ -14,6 +14,7 @@ box_add_and_vagrant_up() {
 
 main() {
   cd ci
+  unset BOSH_LITE_SUBNET_ID
   download_box aws ${GO_PIPELINE_COUNTER}
   box_add_and_vagrant_up aws ${GO_PIPELINE_COUNTER}
   # remotely apt-get update
