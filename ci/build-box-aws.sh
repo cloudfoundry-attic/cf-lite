58147568b5159978f0188c0e8b3d05d3cc25c56a
dmitriy kalinin <dkalinin@pivotal.io>
2015-04-26 21:56:54 -0700

    wip

--- ci/build-box-aws.sh
+++ ci/build-box-aws.sh
@@ -6,6 +6,11 @@ set -e -x
 
 box_version=$(cat box-version/number)
 
+cd cf-lite
+
+export AWS_ACCESS_KEY_ID=$BOSH_AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY=$BOSH_AWS_SECRET_ACCESS_KEY
+
 ./bin/build-aws ${BOSH_LITE_AMI} ${CF_RELEASE_VERSION} $box_version | tee output
 
 ami=`tail -2 output | grep -o "ami-.*"`
